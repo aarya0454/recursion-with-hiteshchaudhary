48103d24db3cddc5577195f1570f07db1f283a18
lakshya <developer.aary@gmail.com>
2025-08-10 22:06:59 +0530

    udnerstand using string reverse in both approach'

--- 1.py
+++ 1.py
@@ -8,3 +8,16 @@ def sum_recursion(arr):
         return 0
     else:
         return arr[0] + sum_recursion(arr[1:])
+
+def rev_string(str):
+    rev_str = ''
+    for char in str:
+        rev_str = char+rev_str
+    return rev_str
+
+def recur_rev_str(s):
+    if len(s)<1:
+        return s
+    else:
+        return recur_rev_str(s[1:]) + s[0] 
+print(recur_rev_str('hello'))
